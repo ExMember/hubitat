c131d51320fcd4f07992963a1294efed6bc9a748
Damien Burke <damien@damienburke.com>
2024-08-31 19:37:13 -0700

    Massive cleanup of existing functionality

--- smartwings-day-night-shades.groovy
+++ smartwings-day-night-shades.groovy
@@ -25,7 +25,6 @@ import groovy.transform.Field
 metadata {
   definition (name: "SmartWings Day/Night Cellular Shades", namespace: "ExMember", author: "Damien Burke") {
     capability "Battery"
-    capability "Configuration"
     capability "WindowShade"
     capability "Refresh"
 
@@ -44,8 +43,7 @@ metadata {
 
 def installed() {
   log.debug "installed()"
-
-  // TODO: request statuses
+  refresh()
 }
 
 def updated() {
@@ -62,7 +60,6 @@ void logsOff(){
 }
 
 def parse(String description) {
-  // log.debug "parse description: $description"
   def cmd = zwave.parse(description, commandClassVersions)
   if (cmd) {
     zwaveEvent(cmd)
@@ -70,79 +67,51 @@ def parse(String description) {
 }
 
 /*
- **********************
- * zwaveEvent Methods *
- **********************
+ *****************************
+ * zwaveEvent Event Handlers *
+ *****************************
  */
 
 def zwaveEvent(hubitat.zwave.commands.basicv1.BasicReport cmd) {
   if (enableDebug) log.debug "BasicReport:  ${cmd}"
 }
 
-def zwaveEvent(hubitat.zwave.commands.supervisionv1.SupervisionGet cmd) {
-   if (logEnable) log.debug "SupervisionGet - SessionID: ${cmd.sessionID}, CC: ${cmd.commandClassIdentifier}, Command: ${cmd.commandIdentifier}"
-   hubitat.zwave.Command encapCmd = cmd.encapsulatedCommand(commandClassVersions)
-   if (encapCmd) {
-      zwaveEvent(encapCmd)
-   }
-   sendHubCommand(new hubitat.device.HubAction(zwaveSecureEncap(zwave.supervisionV1.supervisionReport(sessionID: cmd.sessionID, reserved: 0, moreStatusUpdates: false, status: 0xFF, duration: 0).format()), hubitat.device.Protocol.ZWAVE))
-}
-
 def zwaveEvent(hubitat.zwave.commands.batteryv1.BatteryReport report) {
-  if (logEnable) log.debug "BatteryReport - battery level: ${report.batteryLevel}, payload: ${report.getPayload()}, format: ${report.format()}"
-  state.batteryLevel = report.batteryLevel
+  sendEvent(name: "battery", value: report.batteryLevel, unit: "%")
 }
 
-def zwaveEvent(hubitat.zwave.commands.multichannelv4.MultiChannelEndPointReport report) {
-  if (logEnable) log.debug "MultiChannelEndPointReport - aggregatedEndPoints: ${report.aggregatedEndPoints}, dynamic: ${report.dynamic}, endPoints: ${report.endPoints}, identical: ${report.identical}"
+def zwaveEvent(hubitat.zwave.commands.switchmultilevelv3.SwitchMultilevelReport report) {
+  sendEvent(name: "position", value: report.value, unit: "%")
 }
-
-def zwaveEvent(hubitat.zwave.commands.multichannelv4.MultiChannelEndPointFindReport report) {
-  if (logEnable) log.debug "MultiChannelEndPointFindReport - genericDeviceClass: ${report.genericDeviceClass}, specificDeviceClass: ${report.specificDeviceClass}, reportsToFollow: ${report.reportsToFollow}"
-}
-
 
 def zwaveEvent(hubitat.zwave.Command cmd) {
-  //if (logEnable) log.debug "Ignoring command: ${cmd}"
   log.debug "Ignoring command: ${cmd}"
 }
 
-def topRailPosition(Number position) {
-  log.debug "topRailPosition ${position}"
-  position_set_command = zwave.switchMultilevelV3.switchMultilevelSet(
-    dimmingDuration: 1, value: position
-  )
-  cmd = zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint: 2).encapsulate(position_set_command)
-    
-  cmd.format()
+/*
+ ***********************
+ * Positioning methods *
+ ***********************
+ */
+
+def dualRailPosition(Number position) {
+  railPosition(0, position)
 }
 
 def bottomRailPosition(Number position) {
-  log.debug "bottomRailPosition ${position}"
-  position_set_command = zwave.switchMultilevelV3.switchMultilevelSet(
-    dimmingDuration: 1, value: position
-  )
-  cmd = zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint: 1).encapsulate(position_set_command)
-    
-  cmd.format()
+  railPosition(1, position)
 }
 
-/*
- *************************
- * Configuration methods *
- *************************
- */
-def configure() {
-  log.debug "configure()"
+def topRailPosition(Number position) {
+  railPosition(2, position)
+}
 
-  // def cmd = zwave.multiChannelV4.multiChannelEndPointFind(
-  //     genericDeviceClass: GENERIC_TYPE_SWITCH_MULTILEVEL,
-  //     specificDeviceClass: SPECIFIC_TYPE_CLASS_C_MOTOR_CONTROL
-  //   )
-
-  def cmd = zwave.batteryV1.batteryGet()
-  log.debug "Command batt get: ${cmd.format()}"
-
+def railPosition(Number rail, Number position) {
+  position_set_command = zwave.switchMultilevelV3.switchMultilevelSet(
+    dimmingDuration: 0, value: position
+  )
+  cmd = zwave.multiChannelV3.multiChannelCmdEncap(destinationEndPoint: rail).encapsulate(position_set_command)
+    
   zwaveSecureEncap(cmd.format())
 }
 
@@ -153,21 +122,13 @@ def configure() {
  */
 def refresh() {
   log.debug "Refresh()"
-  // def cmd = zwave.multiChannelV4.multiChannelEndPointFind(
-  //     genericDeviceClass: GENERIC_TYPE_SWITCH_MULTILEVEL,
-  //     specificDeviceClass: SPECIFIC_TYPE_CLASS_C_MOTOR_CONTROL
-  //   )
 
-  def battery_command = zwave.batteryV1.batteryGet()
-  def second_command = zwave.multiChannelV4.multiChannelEndPointGet()
-
-  //log.debug "Refresh command: ${cmd.format()}"
+  sendEvent(name: "battery", value: nil, descriptionText: "Refreshing", isStateChange: true)
+  sendEvent(name: "position", value: nil, descriptionText: "Refreshing", isStateChange: true)
 
   [
-    zwaveSecureEncap(battery_command),
-    zwaveSecureEncap(second_command),
-    zwaveSecureEncap(zwave.multiChannelV4.multiChannelCapabilityGet()),
-    zwaveSecureEncap(zwave.multiChannelV4.multiChannelEndPointFind()),
+    zwaveSecureEncap(zwave.batteryV1.batteryGet()),
+    zwaveSecureEncap(zwave.switchMultilevelV3.switchMultilevelGet()),
   ]
 }
 
@@ -177,64 +138,27 @@ def refresh() {
  ***********************
  */
 def close() {
-  if (logEnable) log.debug "close()"
-
-  def cmd = zwave.switchMultilevelV3.switchMultilevelSet(
-      dimmingDuration: 1, value: 0
-    )
-  log.debug "Command: ${cmd.format()}"
-
-  zwaveSecureEncap(cmd.format())
+  dualRailPosition(0)
 }
 
 def open() {
-  if (logEnable) log.debug "open()"
-
-  def cmd = zwave.switchMultilevelV3.switchMultilevelSet(
-      dimmingDuration: 1, value: 99
-    )
-  log.debug "Command: ${cmd.format()}"
-
-  zwaveSecureEncap(cmd.format())
+  dualRailPosition(99)
 }
+
 def setPosition(Number position) {
-  if (logEnable) log.debug "setPosition(${position})"
-
-  def cmd = zwave.switchMultilevelV3.switchMultilevelSet(
-    dimmingDuration: 1, value: position
-  )
-
-  log.debug "Command: ${cmd.format()}"
-
-  zwaveSecureEncap(cmd.format())
+  dualRailPosition(position)
 }
 
 // direction is "open" or "close"
 def startPositionChange(String direction) {
-  if (logEnable) log.debug "startPositionChange(${direction})"
-
-  def cmd
   if(direction == "open") {
-    cmd = zwave.switchMultilevelV3.switchMultilevelSet(
-      dimmingDuration: 1, value: 0
-    )
+    open()
+  }else if(direction == "close") {
+    close()
   }
-  if(direction == "close") {
-    cmd = zwave.switchMultilevelV3.switchMultilevelSet(
-      dimmingDuration: 1, value: 99
-    )
-  }
-
-  log.debug "Command: ${cmd.format()}"
-
-  zwaveSecureEncap(cmd.format())
 }
 
 def stopPositionChange() {
-  if (logEnable) log.debug "stopPositionChange()"
-
   def cmd = zwave.switchMultilevelV3.switchMultilevelStopLevelChange()
-  log.debug "Command: ${cmd.format()}"
-
   zwaveSecureEncap(cmd.format())
 }
